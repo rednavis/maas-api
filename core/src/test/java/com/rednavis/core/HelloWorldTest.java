92edbfd216087398bddfa9699e503e4551180bcc
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-02-20 10:14:16 +0300

    Added suppor multimodules. Create docker-compose.yml.

---
+++ core/src/test/java/com/rednavis/core/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.rednavis.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+  @Test
+  public void test() {
+    assertEquals(1, 1);
+  }
+}
