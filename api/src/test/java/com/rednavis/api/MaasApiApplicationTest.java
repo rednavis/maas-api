31e29cdeaab33b296637444ea71d591e9532c0aa
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-02-26 12:39:53 +0300

    Fix Gradle build

--- api/src/test/java/com/rednavis/api/MaasApiApplicationTest.java
+++ api/src/test/java/com/rednavis/api/MaasApiApplicationTest.java
@@ -1,13 +1,42 @@
 package com.rednavis.api;
 
+import static java.util.Collections.singletonList;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@ActiveProfiles("test")
 class MaasApiApplicationTest {
+
+  @Container
+  private MongoDbContainer mongoDbContainer = new MongoDbContainer();
+
+  @Bean
+  public MongoClient mongoClient() {
+    MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+        .applyToClusterSettings(settings -> {
+          settings.hosts(singletonList(new ServerAddress(mongoDbContainer.getContainerIpAddress(), mongoDbContainer.getPort())));
+        })
+        .build();
+    return MongoClients.create(mongoClientSettings);
+  }
+
+  @Bean
+  public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+    return new MongoTemplate(mongoClient, "test");
+  }
 
   @Test
   public void contextLoads() {
