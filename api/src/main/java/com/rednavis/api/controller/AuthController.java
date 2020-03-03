796e758dc97fe2a38103769368b812b67e92b7b2
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-03-03 11:31:11 +0300

    Update rest mapping

--- api/src/main/java/com/rednavis/api/controller/AuthController.java
+++ api/src/main/java/com/rednavis/api/controller/AuthController.java
@@ -19,6 +19,7 @@ import com.rednavis.shared.security.CurrentUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(AUTH_URL)
+@RequestMapping(value = AUTH_URL, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class AuthController {
 
   @Autowired
--- api/src/main/java/com/rednavis/api/controller/UserController.java
+++ api/src/main/java/com/rednavis/api/controller/UserController.java
@@ -10,6 +10,7 @@ import com.rednavis.shared.rest.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(USER_URL)
+@RequestMapping(value = USER_URL, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
   @Autowired
